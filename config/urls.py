0a871c6aa4a632cab73ee93c9ed6ea905f9519b8
Alejandro Duque <ducode@outlook.com>
2019-08-11 17:13:36 -0300

    fix production library issue.

--- config/urls.py
+++ config/urls.py
@@ -30,7 +30,6 @@ urlpatterns = [
     path(settings.ADMIN_URL, admin.site.urls),
     path('', include(('weight.users.urls', 'users'), namespace='users')),
     path('', include(('weight.patients.urls', 'patients'), namespace='patients')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
